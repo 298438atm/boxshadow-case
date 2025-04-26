e09ff4ecb4c2e6c56df333136554de31b4c21faf
fujl <2984387807@qq.com>
2025-04-26 21:56:55 +0800

    fjl

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     vue(),
     AutoImport({
